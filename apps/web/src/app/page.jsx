a4f5e917d7a88d45040dec7799671ef47bd8c836
Felixkale <95532883+Felixkale@users.noreply.github.com>
2026-03-05 14:34:49 +0200

    Update page.jsx

--- apps/web/src/app/page.jsx
+++ apps/web/src/app/page.jsx
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Monroy QMS</h1>
-      <a href="/login">Go to Login</a>
-    </div>
-  )
+import { redirect } from "next/navigation";
+
+export default function RootPage() {
+  redirect("/login");
 }
