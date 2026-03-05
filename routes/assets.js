f622f694e78d45936ec41fe09df1a993414ccced
Felixkale <95532883+Felixkale@users.noreply.github.com>
2026-03-05 10:27:53 +0200

    Delete routes directory

--- routes/assets.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/assetsController")
-
-router.get("/", controller.getAssets)
-router.post("/", controller.createAsset)
-router.delete("/:id", controller.deleteAsset)
-
-module.exports = router
