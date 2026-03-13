b9fdd13a65b06eea0b81090bda806919e77fd8ba
Felixkale <95532883+Felixkale@users.noreply.github.com>
2026-03-13 08:36:31 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -20,6 +20,19 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Treat the PDF.js worker as a static file asset so Terser never
+    // tries to minify it (it uses ES module syntax that Terser rejects
+    // when processed as a plain script).
+    config.module.rules.push({
+      test: /pdf\.worker\.(min\.)?mjs$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/worker/[hash][ext][query]",
+      },
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
