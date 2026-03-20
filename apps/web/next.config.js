37c2169db80c05ec48ef0735f37923fcefe31f1c
Felixkale <95532883+Felixkale@users.noreply.github.com>
2026-03-20 12:56:54 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -18,23 +18,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const CopyPlugin = require("copy-webpack-plugin");
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: require.resolve(
-                "pdfjs-dist/build/pdf.worker.min.js"
-              ),
-              to: "../../public/pdf.worker.min.js",
-            },
-          ],
-        })
-      );
-    }
-    return config;
-  },
 };
+
 module.exports = nextConfig;
