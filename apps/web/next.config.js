a54bb9b83958da7f8746d5521b3e3db5734f9dea
Felixkale <95532883+Felixkale@users.noreply.github.com>
2026-03-29 01:13:15 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -18,5 +18,22 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      enforce: "pre",
+      use: [
+        {
+          loader: "string-replace-loader",
+          options: {
+            search: "@import url\\([^)]+\\);?\\s*",
+            replace: "",
+            flags: "g",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 module.exports = nextConfig;
